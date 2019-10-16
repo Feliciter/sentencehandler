5a31c1a20f6293bb914435a3e05eaf8688fa9c0a
Evgeny <17148474+Feliciter@users.noreply.github.com>
2019-10-17 00:05:32 +0300

    Update appdata.service.ts

--- src/app/appdata.service.ts
+++ src/app/appdata.service.ts
@@ -1,49 +1,55 @@
 export class DataService {
-  private data: string[] = [];
-  private workarr: Array<any> = [];
-  private lineArray: Array<string> = [];
-  private csvContent: string;
-  private xmlContent: string;
-  private xmlArray: Array<string> = [];
+         private data: string[] = [];
+         private workarr: Array<any> = [];
+         private lineArray: Array<string> = [];
+         private csvContent: string;
+         private xmlContent: string;
+         private xmlArray: Array<string> = [];
 
-  addData(name: string) {
-    this.data = name.split(".").filter(el => el !== "");
-    this.workarr.length = 0;
+         addData(name: string) {
+           this.data = name.split(".").filter(el => el !== "");
+           this.workarr.length = 0;
 
-    this.data.forEach(item =>
-      this.workarr.push(
-        item
-          .match(/\b(\w+)\b/g)
-          .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
-      )
-    );
-  }
+           this.data.forEach(item =>
+             this.workarr.push(
+               item
+                 .match(/\b(\w+)\b/g)
+                 .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+             )
+           );
+         }
 
-  //  make XML
-  getDataXML() {
-    this.xmlArray.length = 0;
-    for (const sentence of Object.keys(this.workarr)) {
-      this.xmlArray.push("<sentence>");
+         //  make XML
+         getDataXML() {
+           this.xmlArray.length = 0;
+           for (const sentence of Object.keys(this.workarr)) {
+             this.xmlArray.push("<sentence>");
 
-      this.workarr[sentence].forEach(el => {
-        this.xmlArray.push("<word>" + el + "</word>");
-      });
+             this.workarr[sentence].forEach(el => {
+               this.xmlArray.push("<word>" + el + "</word>");
+             });
 
-      this.xmlArray.push("</sentence>");
-    }
-  
-    return ('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'+"<text>"+this.xmlArray.join("") +"</text>" );
-  }
+             this.xmlArray.push("</sentence>");
+           }
+        
+           return (
+             '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' +
+             "<text>" +
+             this.xmlArray.join("") +
+             "</text>"
+           );
+         }
 
-  //  make csv
-  getDataCSV() {
-    this.lineArray.length = 0;
-    for (const m of Object.keys(this.workarr)) {
-      const line = this.workarr[m].join(",");
-      this.lineArray.push(line);
-      this.csvContent = this.lineArray.join("\n");
-    }
+         //  make csv
+         getDataCSV() {
+           this.lineArray.length = 0;
 
-    return this.csvContent;
-  }
-}
+           Object.keys(this.workarr).forEach(el => {
+             const line = this.workarr[el].join(",");
+             this.lineArray.push(line);
+             this.csvContent = this.lineArray.join("\n");
+           });
+
+           return this.csvContent;
+         }
+       }
